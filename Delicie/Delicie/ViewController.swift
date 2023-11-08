aa81378baf02fa7b20df2e6c5dd04dbb557a1e78
mesutgdk <mesuttgedik@gmail.com>
2023-11-08 15:55:25 +0300

    feat: without stroyboard

--- Delicie/Delicie/ViewController.swift
+++ Delicie/Delicie/ViewController.swift
@@ -9,10 +9,28 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let dummyLabel : UILabel = {
+       let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "dummy Label"
+        label.textColor = .systemRed
+        label.font = UIFont.systemFont(ofSize: 24)
+        
+        return label
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        view.addSubview(dummyLabel)
+        
+        NSLayoutConstraint.activate([
+            dummyLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            dummyLabel.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+        ])
+        
     }
+    
 
 
 }
