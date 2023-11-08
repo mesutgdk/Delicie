8d444838d9d0f72d0e480881371d1da26d80ea16
mesutgdk <mesuttgedik@gmail.com>
2023-11-08 14:54:32 +0300

    doc: new project to do a foot order app

---
+++ Delicie/Delicie/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Delicie
+//
+//  Created by Mesut Gedik on 8.11.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
