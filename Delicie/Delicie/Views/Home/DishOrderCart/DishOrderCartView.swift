38ec4591f6d0e39e68d2e609480ff62f4ab993ae
mesutgdk <mesuttgedik@gmail.com>
2024-01-23 23:24:47 +0300

    doc: relocate files, rename for better understanding, feat: dishorderVC and view

---
+++ Delicie/Delicie/Views/Home/DishOrderCart/DishOrderCartView.swift
@@ -0,0 +1,20 @@
+//
+//  DishOrderCartView.swift
+//  Delicie
+//
+//  Created by Mesut Gedik on 23.01.2024.
+//
+
+import UIKit
+
+class DishOrderCartView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
