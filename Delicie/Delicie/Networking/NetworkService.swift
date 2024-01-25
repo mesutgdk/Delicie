d0d5fc8471b39deef9c39314eab26330b2b64661
mesutgdk <mesuttgedik@gmail.com>
2024-01-25 09:12:23 +0300

    feat: networking func

--- Delicie/Delicie/Networking/NetworkService.swift
+++ Delicie/Delicie/Networking/NetworkService.swift
@@ -12,9 +12,31 @@ struct NetworkService{
     /// CreatingRequest
     /// route: path to endpoint
     /// method: type of request
-    private func createRequest(route: Route, method: Method, parameters: [String:Any] = nil) -> URLRequest? {
+    private func createRequest(route: Route, 
+                               method: Method,
+                               parameters: [String:Any]? = nil) -> URLRequest? {
         
         let urlString = Route.baseURL + route.descripton
         guard let url = urlString.asUrl else { return nil }
+        var urlRequest = URLRequest(url: url)
+        urlRequest.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        urlRequest.httpMethod = method.rawValue
+        
+        if let params = parameters {
+            switch method {
+            case .get:
+                var urlComponent = URLComponents(string: urlString)
+                urlComponent?.queryItems = params.map {
+                    URLQueryItem(name: $0, value: "\($1)")
+                }
+                urlRequest.url = urlComponent?.url
+            case .post, .delete, .patch:
+                let bodyData = try?
+                JSONSerialization.data(withJSONObject: params, options: [])
+                urlRequest.httpBody = bodyData
+            }
+        }
+        return urlRequest
+        
     }
 }
