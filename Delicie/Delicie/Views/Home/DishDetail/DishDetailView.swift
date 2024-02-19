29d0bad7793e82518dceafc02438a4a9621d4280
mesutgdk <mesuttgedik@gmail.com>
2024-02-19 16:29:18 +0300

    feat: order button revision

--- Delicie/Delicie/Views/Home/DishDetail/DishDetailView.swift
+++ Delicie/Delicie/Views/Home/DishDetail/DishDetailView.swift
@@ -102,9 +102,8 @@ final class DishDetailView: UIView {
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
+    
     // MARK: - Setup & Layout
-    
-    
     private func setup(){
         translatesAutoresizingMaskIntoConstraints = false
         backgroundColor = .systemBackground
@@ -206,6 +205,7 @@ extension DishDetailView{
             switch result {
             case .success(let order):
                 ProgressHUD.succeed("Your order has been received.🍽️"+(order.name ?? ""))
+                
             case .failure(let error):
                 ProgressHUD.error(error.localizedDescription)
             }
